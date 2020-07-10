13dd6edfaca834585558810f946506861b0d3203
lilah <xxami@users.noreply.github.com>
2020-07-10 19:18:02 +0100

    link with libm, print entrypoint of helloworld app

--- src/xdi.c
+++ src/xdi.c
@@ -1,8 +1,28 @@
 
 #include <stdio.h>
-#include <pe.h>
+#include <libpe/pe.h>
+
+const char* hello_world = "test_applications/hello-world.exe";
 
 int main(void) {
-  printf("Hello, world!\n");
+  pe_ctx_t ctx;
+  pe_err_e err = pe_load_file(&ctx, hello_world);
+  if (err != LIBPE_E_OK) {
+    pe_error_print(stderr, err);
+    return 1;
+  }
+
+  err = pe_parse(&ctx);
+  if (err != LIBPE_E_OK) {
+    pe_error_print(stderr, err);
+    return 1;
+  }
+
+  if (!pe_is_pe(&ctx)) {
+    printf("Unexpected error parsing PE file");
+    return 1;
+  }
+
+  printf("Entrypoint: %#lx\n", ctx.pe.entrypoint);
 	return 0;
 }
